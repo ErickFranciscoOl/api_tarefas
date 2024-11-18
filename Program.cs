a8e7a081006e94de11f04f7d3ddf15280bd041aa
Erick Francisco de Oliveira <161622308+ErickFranciscoOl@users.noreply.github.com>
2024-11-18 12:44:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using TarefasAPI.Contexto;
+using TarefasAPI.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ITarefasRepository, TarefasRepository>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
